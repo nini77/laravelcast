26c4e378aa4ff2b345d58ac789a1897a7466e555
nini <testabcdLavse@mail.com>
2015-06-04 11:15:04 +0900

    append h2

--- resources/views/articles/createConfirm.blade.php
+++ resources/views/articles/createConfirm.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
   <h1>New Article Confirm </h1>
+  <h2>Please Cofirm Your Article</h2>
   <hr/>
   {!! Form::model($article = new \App\Articles, ['method' => 'post','url' => 'articles' ]) !!}
       @include('articles.form',['submitClass' => 'btn btn-primary form-control','submitButtonText' => 'Add Article', 'readOnly' =>'readOnly','id' => 'createArticlebtn'])
