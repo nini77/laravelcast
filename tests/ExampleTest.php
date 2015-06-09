094d9513d038f8720d5391d61b84231dd7222534
nini <testabcdLavse@mail.com>
2015-06-09 15:56:53 +0900

    add testcd

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,17 +1,85 @@
 <?php
+use Laracasts\Integrated\Extensions\Laravel as IntegrationTest;
+
+use Laracasts\TestDummy\Factory as TestDummy;
 
 class ExampleTest extends TestCase {
 
-	/**
-	 * A basic functional test example.
-	 *
-	 * @return void
-	 */
+	/** @test */
 	public function testBasicExample()
 	{
-		$response = $this->call('GET', '/');
 
-		$this->assertEquals(200, $response->getStatusCode());
+		$this->visit('/');
+
+	}
+
+	/** @test */
+	public function it_visit_articles()
+	{
+			$this->visit('/articles')
+					->see('Synchro Articles');
+	}
+
+	/** @test */
+	public function it_display_all_posts()
+	{
+		TestDummy::Create('App\Articles',['title' => 'gesu']);
+
+		$this->visit('/articles')
+				->andSee('gesu');
+
+	}
+
+		/** @test */
+	public function it_login()
+	{
+		// login
+		$this->visit('/articles')
+					->click('Login')
+					->andSee('Forgot Your Password')
+					->type('wiki@mail.com','email')
+					->type('wikiwiki','password')
+					->press('Login')
+					->andSee('Logout');
+	}
+
+		/** @test */
+	public function it_create_articles_until_confirm()
+	{
+		$this->visit('/articles')
+					->click('Login')
+					->andSee('Forgot Your Password')
+					->type('wiki@mail.com','email')
+					->type('wikiwiki','password')
+					->press('Login')
+					->visit('/articles/create')
+					->andSee('Write a New Article')
+					->type('title','title')
+					->type('titleUnitTest','title')
+					->type('bodyUnitTest','body')
+					->select('tag_list','2')
+					->andPress('Confirm Article')
+					->andSee('Please Cofirm Your Article');
+	}
+
+		/** @test */
+	public function it_create_articles()
+	{
+		$this->visit('/articles')
+					->click('Login')
+					->andSee('Forgot Your Password')
+					->type('wiki@mail.com','email')
+					->type('wikiwiki','password')
+					->press('Login')
+					->visit('/articles/create')
+					->andSee('Write a New Article')
+					->type('title','title')
+					->type('titleUnitTest','title')
+					->type('bodyUnitTest','body')
+					->select('tag_list','2')
+					->andPress('Confirm Article')
+					->andSee('Please Cofirm Your Article')
+					->press('Add Article');
 	}
 
 }
